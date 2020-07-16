a754fc246ab777c0867f73c3aa8d84e0436715c6
Namrah96 <64134837+Namrah96@users.noreply.github.com>
2020-07-16 20:06:12 +0500

    addtocart

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useContext } from "react";
 import {
     BrowserRouter as Router,
     Routes,
@@ -9,13 +10,13 @@ import {
 } from "react-router-dom";
 import '../Navbar/Navbar.css';
 import AppSlider from '../Slider/AppSlider';
-
+import GlobalContext from "../../state/GlobalContext";
 import shoesW from '../../shoesW.json';
 import shoesM from '../../shoesM.json';
 // import AllProducts from './AllProducts';
-import { Container } from '@material-ui/core';
+import { Container, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 
 
@@ -70,7 +71,6 @@ function Launcha() {
             backgroundColor: '#569496',
             padding: '70px 0px 20px 20px',
             color: 'white'
-
         },
     }))
 
@@ -79,8 +79,8 @@ function Launcha() {
     return (
         <Container maxWidth='lg'>
             <div className={classes.title}>
-                <h1><u>Products</u></h1>
-                <h3>WOMEN FOOTWEAR</h3>
+                <h1><u>PRODUCTS</u></h1>
+                <h2>WOMEN FOOTWEAR</h2>
             </div>
             <hr />
             <br />
@@ -90,6 +90,19 @@ function Launcha() {
 }
 
 function LaunchIndexa() {
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            marginTop: theme.spacing(3),
+            backgroundColor: "#2a6070"
+        }
+    }));
+
+    const { addToCart } = useContext(GlobalContext);
+    function handleAddToCart() {
+        addToCart(shoesW);
+    }
+
+    const classes = useStyles();
     return (
         <div className="productContainer">
             {Object.keys(shoesW).map(keyName => {
@@ -98,12 +111,23 @@ function LaunchIndexa() {
                     <Link key={keyName}
                         className="link" to={`/women/${keyName}`}>
                         <img src={shoe.img} height={250} alt={shoe.name} />
-                        <h4>{shoe.name}</h4>
+                        <h3>{shoe.name}</h3>
                         <h4>Price: {shoe.price}</h4>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            startIcon={<AddShoppingCartIcon />}
+                            onClick={handleAddToCart}
+                            disabled={shoe.items_left <= 0}
+                            fullWidth
+                        >
+                            Add to cart
+                        </Button>
                     </Link>)
             })}
         </div>
-        
+
     );
 }
 
@@ -111,20 +135,57 @@ function LaunchShoea() {
     const { slug } = useParams();
     const shoe = shoesW[slug];
 
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            marginTop: theme.spacing(3),
+            backgroundColor: "#2a6070"
+        }
+    }));
+
+    const { addToCart } = useContext(GlobalContext);
+
+    const classes = useStyles();
+
     if (!shoe) {
         return <h2>Product Not Found!</h2>;
     }
 
     const { name, img } = shoe;
 
+    function handleAddToCart() {
+        addToCart(shoesM);
+    }
+
     return (
         <div>
-            <h1>Women Collection </h1>
+            <h1 color="black">Women Collection </h1>
+            <br />
             <div className="productContainer">
-                <h2>{shoe.name}</h2>
-                <br/>
-                <div><h2>{shoe.price}</h2></div>
-                <img src={shoe.img} alt={name} />
+                <ul>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <li><h2>{shoe.name}</h2></li>
+                    <br />
+                    <li><h3>Price: {shoe.price}</h3></li>
+                    <Button
+                        variant="contained"
+                        color="inherit"
+                        className={classes.button}
+                        startIcon={<AddShoppingCartIcon />}
+                        onClick={handleAddToCart}
+                        disabled={shoe.items_left <= 0}
+                        fullWidth
+                    >
+                        Add to cart
+                    </Button>
+                </ul>
+                <br />
+                <br />
+                <br />
+                <br />
+                <img src={shoe.img} alt={name} height={550}/>
             </div>
         </div>
     );
@@ -144,8 +205,8 @@ function Launchb() {
     return (
         <Container maxWidth='lg'>
             <div className={classes.title}>
-                <h1><u>Products</u></h1>
-                <h3>MEN FOOTWEAR</h3>
+                <h1><u>ProRODUCTS</u></h1>
+                <h2>MEN FOOTWEAR</h2>
             </div>
             <hr />
             <br />
@@ -155,6 +216,20 @@ function Launchb() {
 }
 
 function LaunchIndexb() {
+
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            marginTop: theme.spacing(3),
+            backgroundColor: "#2a6070"
+        }
+    }));
+
+    const { addToCart } = useContext(GlobalContext);
+    function handleAddToCart() {
+        addToCart(shoesM);
+    }
+
+    const classes = useStyles();
     return (
         <div className="productContainer">
             {Object.keys(shoesM).map(keyName => {
@@ -163,8 +238,19 @@ function LaunchIndexb() {
                     <Link key={keyName}
                         className="link" to={`/men/${keyName}`}>
                         <img src={shoe.img} height={270} alt={shoe.name} />
-                        <h4>{shoe.name}</h4>
+                        <h3>{shoe.name}</h3>
                         <h4>Price: {shoe.price}</h4>
+                        <Button
+                            variant="contained"
+                            color="inherit"
+                            className={classes.button}
+                            startIcon={<AddShoppingCartIcon />}
+                            onClick={handleAddToCart}
+                            disabled={shoe.items_left <= 0}
+                            fullWidth
+                        >
+                            Add to cart
+                        </Button>
                     </Link>)
             })}
         </div>
@@ -175,18 +261,56 @@ function LaunchShoeb() {
     const { slug } = useParams();
     const shoe = shoesM[slug];
 
+    const useStyles = makeStyles((theme) => ({
+        button: {
+            marginTop: theme.spacing(3),
+            backgroundColor: "#2a6070"
+        }
+    }));
+
+    const { addToCart } = useContext(GlobalContext);
+
+    const classes = useStyles();
+
     if (!shoe) {
         return <h2>Product Not Found!</h2>;
     }
 
     const { name, img } = shoe;
 
+    function handleAddToCart() {
+        addToCart(shoesM);
+    }
+
     return (
         <div>
-            <h1>Men Collection </h1>
+            <h1 color="black">Men Collection </h1>
+            <br/>
             <div className="productContainer">
-                <h2>{shoe.name}</h2>
-                <img src={shoe.img} alt={name} />
+                <ul>
+                    <br />
+                    <br />
+                    <li><h2>{shoe.name}</h2></li>
+                    <br />
+                    <li><h3>Price: {shoe.price}</h3></li>
+                    <Button
+                        variant="contained"
+                        color="inherit"
+                        className={classes.button}
+                        startIcon={<AddShoppingCartIcon />}
+                        onClick={handleAddToCart}
+                        disabled={shoe.items_left <= 0}
+                        fullWidth
+                    >
+                        Add to cart
+                </Button>
+                </ul>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <img src={shoe.img} alt={name} height={550} />
             </div>
         </div>
     );
